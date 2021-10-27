265e4be55f2aac348ddb1f8f1740d2db287970fd
Simon Grchevski <simongrcevski@gmail.com>
2021-10-27 22:29:45 +0200

    Fix rubocop errors

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,27 +1,27 @@
-require "rails_helper"
+require 'rails_helper'
 RSpec.describe 'Users', type: :request do
-    #index
+  # index
   describe 'GET #index' do
     before(:example) { get users_path }
-    it "is a success" do
+    it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
     it "renders 'index' template" do
       expect(response).to render_template('index')
     end
 
-    it "renders title" do
-      expect(response.body).to include("Users INDEX") 
+    it 'renders title' do
+      expect(response.body).to include('Users INDEX')
     end
-    it "renders title" do
-      expect(response.body).to have_tag("h2") 
+    it 'renders title' do
+      expect(response.body).to have_tag('h2')
     end
   end
 
-  #show
+  # show
   describe 'GET #show' do
     before(:example) { get '/users/:show' }
-    it "is a success" do
+    it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
 
@@ -29,9 +29,8 @@ RSpec.describe 'Users', type: :request do
       expect(response).to render_template('show')
     end
 
-    it "renders title" do
-      expect(response.body).to have_tag("h3") 
+    it 'renders title' do
+      expect(response.body).to have_tag('h3')
     end
   end
-
 end
