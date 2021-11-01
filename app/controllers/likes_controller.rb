cbbbc0a44f2b44c0c1012958ba1cfdc066ab4208
Simon Grchevski <simongrcevski@gmail.com>
2021-11-01 17:58:46 +0100

    Fix linter errors

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,5 @@
 class LikesController < ApplicationController
   def index
-    Like.update_like_counter(:post_id => 1, :update_to => 3)
+    Like.update_like_counter(post_id: 1, update_to: 3)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    Post.update_post_counter(current_user:current_user.name, update_to:10)
+    Post.update_post_counter(current_user: current_user.name, update_to: 10)
     @posts = Post.last_comments(1)
   end
 
