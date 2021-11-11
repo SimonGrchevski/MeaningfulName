fa5e345763c2b092d1eaeb73ba4bd90b2e1d5623
Simon Grchevski <simongrcevski@gmail.com>
2021-11-11 15:57:58 +0100

    Add basic styles

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,9 +1,4 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
-
-  # def self.update_like_counter(param)
-  #   @post = Post.find_by_id(param[:post_id])
-  #   @post.update(likesCounter: param[:update_to])
-  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:passwords => 'passwords'}
 
   
   # get 'comments/index'
